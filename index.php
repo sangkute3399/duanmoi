2383366bb42fe903500379069ae74daddff9cdb5
sa <sangkute3399@gmail.com>
2025-11-29 20:38:49 +0700

    lan 1

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// Đây là Controller đơn giản nhất (Front Controller)
+// Gọi View hiển thị ra màn hình
+require_once 'views/home.php';
+?>
---
+++ views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello Ubuntu 24</title>
+</head>
+<body>
+    <h1>Xin chào Ubuntu 24.04 LTS!</h1>
+    <p>Code này đang chạy trên XAMPP Linux.</p>
+</body>
+</html>
